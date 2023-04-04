84fbda58f48a64c7c55de7cd6ebc207a6044eb2d
Dmambo <mambotrader0@gmail.com>
2023-04-04 16:57:35 +0530

    Fix linters

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,6 @@
 class Comment < ApplicationRecord
-    belongs_to :user, foreign_key: 'author_id'
-    belongs_to :post
-  
-    validates :text, presence: true
-  end
-  
+  belongs_to :user, foreign_key: 'author_id'
+  belongs_to :post
+
+  validates :text, presence: true
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
-    has_many :posts, dependent: :destroy
-    has_many :likes, dependent: :destroy
-    has_many :comments, dependent: :destroy
-  end
-  
+  has_many :posts, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :comments, dependent: :destroy
+end
