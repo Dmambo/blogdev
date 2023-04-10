1f3e4e6c8101e47668ee92d36f9642d2175113a1
Dmambo <mambotrader0@gmail.com>
2023-04-10 14:46:54 +0530

    Created a controller for the blog app

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  root 'pages#hello'
+  # root 'pages#hello'
+
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show], module: :users
+  end
+  
+  resources :posts, only: [:show], module: :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
