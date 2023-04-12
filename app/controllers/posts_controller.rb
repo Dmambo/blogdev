1e0aab805c39aa31ce61989f791dd28eb142135c
Dmambo <mambotrader0@gmail.com>
2023-04-12 22:39:43 +0530

    Added the view file and style it

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,13 @@
 class PostsController < ApplicationController
-  def index; end
+  def index
+    @users = User.find(params[:id])
+    @posts = @users.posts.includes(:comments, :likes)
+  end
 
-  def show; end
+  def show
+    @users = User.find(params[:id])
+    @post = Post.find(params[:id_post])
+    @comments = @post.comments.includes(:author)
+    @likes = @post.likes.includes(:author)
+  end
 end
