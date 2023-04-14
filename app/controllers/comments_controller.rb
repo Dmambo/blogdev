9f53c5a7904667cbe62c03def72a849f727cf35d
Dmambo <mambotrader0@gmail.com>
2023-04-14 12:24:58 +0530

    Added form for the blog

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,19 @@
+class CommentsController < ApplicationController
+
+  def new
+    @comment = Comment.new
+  end
+  
+    def create
+      @comment = @current_user.comments.new(params)
+      @comment.post_id = params[:post_id]
+        redirect_to @post
+      end
+  
+    private
+  
+    def comment_params
+      params.require(:comment).permit(:text)
+    end
+  end
+  
---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,8 @@
+class LikesController < ApplicationController
+    def create
+        @post = Post.find(params[:post_id])
+        @like = current_user.likes.create(post: @post)
+        redirect_to @post
+      end
+  end
+  
