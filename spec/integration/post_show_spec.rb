2d94e4084040560691b2f10e423bc2c7e9a3e8e3
Utsargo Roy <royutsargo@gmail.com>
2023-04-19 19:04:02 +0600

    Fix linter errors

--- spec/integration/post_show_spec.rb
+++ spec/integration/post_show_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 RSpec.feature 'Post show page', type: :feature do
-    let!(:user) { User.create!(name: 'Tom', bio: 'Teacher from Mexico', posts_counter: 0) }
-    let!(:post1) { Post.create!(title: 'Hello', content: 'This is my first post', author: user) }
-    let!(:comment1) { Comment.create!(text: 'hi there', post: post1, author: user) }
-    let!(:comment2) { Comment.create!(text: 'Thanks for sharing', post: post1, author: user) }
-    let!(:like1) { Like.create!(post: post1, author: user) }
+  let!(:user) { User.create!(name: 'Tom', bio: 'Teacher from Mexico', posts_counter: 0) }
+  let!(:post1) { Post.create!(title: 'Hello', content: 'This is my first post', author: user) }
+  let!(:comment1) { Comment.create!(text: 'hi there', post: post1, author: user) }
+  let!(:comment2) { Comment.create!(text: 'Thanks for sharing', post: post1, author: user) }
+  let!(:like1) { Like.create!(post: post1, author: user) }
 
   scenario 'user visits the post show page' do
     visit user_post_path(user, post1)
@@ -17,8 +17,8 @@ RSpec.feature 'Post show page', type: :feature do
     expect(page).to have_content post1.text
 
     post1.comments.each do |comment|
-        expect(page).to have_content comment.author.name
-        expect(page).to have_content comment.text
-      end
+      expect(page).to have_content comment.author.name
+      expect(page).to have_content comment.text
+    end
   end
 end
--- spec/integration/user_index_spec.rb
+++ spec/integration/user_index_spec.rb
@@ -1,23 +1,22 @@
 require 'rails_helper'
 require 'capybara/rails'
 
-RSpec.feature "User index page", type: :feature do
-  let!(:user1) { User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')}
-  
-  scenario "shows the username of all other users" do
+RSpec.feature 'User index page', type: :feature do
+  let!(:user1) { User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.') }
+
+  scenario 'shows the username of all other users' do
     visit users_path
-        expect(page).to have_content(user1.name)
+    expect(page).to have_content(user1.name)
   end
-  
-  scenario "shows the profile picture for each user" do
+
+  scenario 'shows the profile picture for each user' do
     visit users_path
     expect(page).to have_selector("img[src='#{user1.photo}']")
   end
-  
-  scenario "shows the number of posts each user has written" do
-    post1 = Post.create(author_id: user1.id, title: "Hello")
 
-    
+  scenario 'shows the number of posts each user has written' do
+    Post.create(author_id: user1.id, title: 'Hello')
+
     visit users_path
     expect(page).to have_content("Number of post: #{user1.posts.count}", count: 0)
   end
